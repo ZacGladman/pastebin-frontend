cfcac59b6ca4deb9e308f320a931def799b21f17
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-07 23:12:00 +0000

    reset input fields on submission

--- src/components/MainContentFiles/SingleSummary.tsx
+++ src/components/MainContentFiles/SingleSummary.tsx
@@ -93,6 +93,7 @@ export default function SingleSummary({
             onSubmit={(e) => {
               handleSubmitComment();
               e.preventDefault();
+              setInputComment({ username: "", comment: "" });
             }}
           >
             <input
