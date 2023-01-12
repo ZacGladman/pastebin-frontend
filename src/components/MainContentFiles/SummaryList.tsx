8969c9d55acc1605e2233852316717c27a541a85
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-12 19:42:03 +0000

    add loading spin

--- src/components/MainContentFiles/SummaryList.tsx
+++ src/components/MainContentFiles/SummaryList.tsx
@@ -1,5 +1,6 @@
 import { ISummaryList } from "../../utils/interfaces";
 import SingleSummary from "./SingleSummary";
+import LoadingSpin from "react-loading-spin";
 
 export default function SummaryList({
   fetchedPastes,
@@ -11,24 +12,33 @@ export default function SummaryList({
 }: ISummaryList): JSX.Element {
   console.log(fetchedPastes);
 
-  return (
-    <div className="summaryListContainer">
-      <p className="usageInstructions">
-        {">"} click on a paste to see its comments {"<"}
-      </p>
-      {fetchedPastes.map((paste) => {
-        return (
-          <SingleSummary
-            paste={paste}
-            key={paste.id}
-            isActive={isActive === paste.id}
-            setActiveIndex={setActiveIndex}
-            singleSummaryIndex={singleSummaryIndex}
-            setSingleSummaryIndex={setSingleSummaryIndex}
-            fetchComments={fetchComments}
-          />
-        );
-      })}
-    </div>
-  );
+  if (fetchedPastes) {
+    return (
+      <div className="summaryListContainer">
+        <p className="usageInstructions">
+          {">"} click on a paste to see its comments {"<"}
+        </p>
+        {fetchedPastes.map((paste) => {
+          return (
+            <SingleSummary
+              paste={paste}
+              key={paste.id}
+              isActive={isActive === paste.id}
+              setActiveIndex={setActiveIndex}
+              singleSummaryIndex={singleSummaryIndex}
+              setSingleSummaryIndex={setSingleSummaryIndex}
+              fetchComments={fetchComments}
+            />
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Fetching Data - render.com may take some time to wake up! 💤</h1>
+        <LoadingSpin />
+      </div>
+    );
+  }
 }
